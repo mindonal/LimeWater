d62134a5301e9188520be6e56a24a768f897cbee
bk.ko <bk.ko@sk.com>
2015-09-14 11:30:21 +0900

    setup Entity

---
+++ src/main/java/com/limewater/LimeWaterApplication.java
@@ -0,0 +1,14 @@
+package com.limewater;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by 1002311 on 9/14/15.
+ */
+@SpringBootApplication
+public class LimeWaterApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LimeWaterApplication.class, args);
+    }
+}
